63768960beb20925803693d068aed16d42d57f2c
unknown <grantludwig97@gmail.com>
2019-05-16 17:42:50 -0700

    created is valid table

--- hw5.fs
+++ hw5.fs
@@ -3,3 +3,16 @@
     |[] -> 0
     |hd::tl -> 
         if hd % 2 = 0 then hd + sumEven tl else sumEven tl
+
+let rec inList list value =
+    match list with
+    |[] -> false
+    |hd::tl ->
+        if value = hd then true else inList tl value
+
+let rec isValidTable constraints list =
+    match constraints with
+    |[] -> true
+    |hd::tl -> 
+        let (name1, name2) = hd 
+        if inList list name1 && inList list name2 then false else isValidTable tl list
